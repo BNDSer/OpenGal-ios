0b395246704e497f3b6ccd69e826e87cabb3c842
BNDSer <zichen.feng@outlook.com>
2026-06-16 20:30:00 +0800

    Initial Commit

---
+++ OpenGal-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenGal-ios
+//
+//  Created by Zichen on 2026/6/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
